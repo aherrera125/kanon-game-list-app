d6cd075d15960b2c7b3c26e5d9ea53042fa0d320
aherrera125 <j.alejandroherrera@gmail.com>
2024-10-21 12:41:55 +0200

    Add search functionality and refactor code

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react';
 
 function App() {    
   const [data, setData] = useState(null);  
+  const [gameText, setGameText] = useState('');
+
   useEffect(() => {
     fetch('/game-data.json')
       .then(response => response.json())
@@ -14,11 +16,22 @@ function App() {
     return <div>Loading Data...</div>;
   }
 
+  const handleSearch = (event) => {   
+    setGameText(event.target.value);
+  };
+
+  const filteredGames = data.filter((game)=>{      
+    return game.title.toLowerCase().includes(gameText.toLowerCase())
+  })
+
+  const gamesToDisplay = gameText ? filteredGames : data;
+
   return (
     <div className="App centerElement">
+      <input type='text' placeholder='Type kind of game' value={gameText} onChange={handleSearch}></input>
       <h1>Game List:</h1>
       <ul>
-        {data.map((item) => (
+        {gamesToDisplay.map((item) => (
           <li key={item.id}>
             <p>Title: <b>{item.title}</b></p>
             <p>Provider Name: <b>{item.providerName}</b></p>
