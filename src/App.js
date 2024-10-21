1e78f9b3c830ecc326c971329230c6024f9558c4
aherrera125 <j.alejandroherrera@gmail.com>
2024-10-21 10:51:09 +0200

    Improve code and refactor App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
-import data from '../src/game-data.json';
+import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {  
+function App() {    
+  const [data, setData] = useState(null);  
+  useEffect(() => {
+    fetch('/game-data.json')
+      .then(response => response.json())
+      .then(jsonData => setData(jsonData))
+      .catch(error => console.error('Error to loading JSON:', error));
+  }, []);
+
+  if (!data) {
+    return <div>Loading Data...</div>;
+  }
 
   return (
-    <div className="App">
+    <div className="App centerElement">
       <h1>Game List:</h1>
       <ul>
         {data.map((item) => (
