fade1a9b235e8d2788975f5d3b980bed76ce9bf0
aherrera125 <j.alejandroherrera@gmail.com>
2024-10-23 13:01:02 +0200

    Refactory - Add componensts js and css

--- src/App.js
+++ src/App.js
@@ -1,45 +1,13 @@
+import React from "react";
+import GameList from "./components/GameList";
 import './App.css';
-import React, { useEffect, useState } from 'react';
 
-function App() {    
-  const [data, setData] = useState(null);  
-  const [gameText, setGameText] = useState('');
-
-  useEffect(() => {
-    fetch('/game-data.json')
-      .then(response => response.json())
-      .then(jsonData => setData(jsonData))
-      .catch(error => console.error('Error to loading JSON:', error));
-  }, []);
-
-  if (!data) {
-    return <div>Loading Data...</div>;
-  }
-
-  const handleSearch = (event) => {   
-    setGameText(event.target.value);
-  };
-
-  const filteredGames = data.filter((game)=>{      
-    return game.title.toLowerCase().includes(gameText.toLowerCase())
-  })
-
-  const gamesToDisplay = gameText ? filteredGames : data;
-
+function App() {
   return (
-    <div className="App centerElement">
-      <input type='text' placeholder='Type kind of game' value={gameText} onChange={handleSearch}></input>
-      <h1>Game List:</h1>
-      <ul>
-        {gamesToDisplay.map((item) => (
-          <li key={item.id}>
-            <p>Title: <b>{item.title}</b></p>
-            <p>Provider Name: <b>{item.providerName}</b></p>
-          </li>
-        ))}
-      </ul>
+    <div className="App">
+      <GameList />
     </div>
   );
-};
+}
 
 export default App;
---
+++ src/components/GameList.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import './GameList.css';
+
+const GameList = () => {
+    const [data, setData] = useState(null);
+    const [gameText, setGameText] = useState('');
+
+    useEffect(() => {
+        fetch('/game-data.json')
+            .then(response => response.json())
+            .then(jsonData => setData(jsonData))
+            .catch(error => console.error('Error to loading JSON:', error));
+    }, []);
+
+    if (!data) {
+        return <div>Loading Data...</div>;
+    }
+
+    const handleSearch = (event) => {
+        setGameText(event.target.value);
+    };
+
+    const filteredGames = data.filter((game) => {
+        return game.title.toLowerCase().includes(gameText.toLowerCase())
+    })
+
+    const gamesToDisplay = gameText ? filteredGames : data;
+
+    return (
+        <div className="App centerElement">
+            <input type='text' placeholder='Type kind of game' value={gameText} onChange={handleSearch}></input>
+            <h1>Game List:</h1>
+            <ul>
+                {gamesToDisplay.map((item) => (
+                    <li key={item.id}>
+                        <p>Title: <b>{item.title}</b></p>
+                        <p>Provider Name: <b>{item.providerName}</b></p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );    
+};
+
+export default GameList;
